107e0f4635cff8686cd1bfd609bd51d3a25bb293
wangfeng00390438 <wang_feng7@want-want.com>
2020-11-07 14:29:17 +0800

    vue2+cli4搭建简易后台管理系统

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,66 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <el-container>
+      <el-aside width="200px">
+        <el-menu :default-active="$route.path" router>
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-message"></i>导航一
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="/home">
+                Home
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-menu"></i>导航二
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="/about">
+                About
+              </el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main><router-view /></el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<script>
+export default {
+  created() {
+    console.log(this.$route.path);
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.el-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
   text-align: center;
-  color: #2c3e50;
+  line-height: 60px;
 }
 
-#nav {
-  padding: 30px;
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
 }
 </style>
